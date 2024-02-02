30c22a9c6de1e41158f224ee5d3307bf62530973
peddyrbeg <85888409+peddyrbeg@users.noreply.github.com>
2024-02-02 20:56:10 +0000

    Add files via upload

---
+++ fysseree/focklyn.js
@@ -0,0 +1,46 @@
+const focklynYnsit = {
+    "kayt": {
+        caslys: "caslyssyn/kayt.webp",
+        sheean: "sheeanyn/kayt.mp3"
+    },
+    "moddey": {
+        caslys: "caslyssyn/moddey.webp",
+        sheean: "sheeanyn/moddey.mp3"
+    },
+    "eeast": {
+        caslys: "caslyssyn/eeast.webp",
+        sheean: "sheeanyn/eeast.mp3"
+    },
+    "jerbyl": {
+        caslys: "caslyssyn/jerbyl.webp",
+        sheean: "sheeanyn/jerbyl.mp3"
+    },
+    "pussag": {
+        caslys: "caslyssyn/pussag.webp",
+        sheean: "sheeanyn/pussag.mp3"
+    },
+    "lugh": {
+        caslys: "caslyssyn/lugh.webp",
+        sheean: "sheeanyn/lugh.mp3"
+    },
+    "conning": {
+        caslys: "caslyssyn/conning.webp",
+        sheean: "sheeanyn/conning.mp3"
+    },
+    "garran": {
+        caslys: "caslyssyn/garran.webp",
+        sheean: "sheeanyn/garran.mp3"
+    },
+    "ushag": {
+        caslys: "caslyssyn/ushag.webp",
+        sheean: "sheeanyn/ushag.mp3"
+    },
+    "shligginagh": {
+        caslys: "caslyssyn/shligginagh.webp",
+        sheean: "sheeanyn/shligginagh.mp3"
+    },
+    "ardnieu": {
+        caslys: "caslyssyn/ardnieu.webp",
+        sheean: "sheeanyn/ardnieu.mp3"
+    }
+};
